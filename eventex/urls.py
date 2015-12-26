3885785b7434dff9d1dd5f0e52e8e30e023efd3b
Juliano Barbosa <Julianomb@gmail.com>
2015-12-26 16:55:00 -0200

    Atualiza urls para 1.9

--- eventex/urls.py
+++ eventex/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
+from eventex.core.views import home
+
 urlpatterns = [
-    url(r'^$', 'eventex.core.views.home'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
+    url(r'^admin/', include(admin.site.urls)),
 ]
