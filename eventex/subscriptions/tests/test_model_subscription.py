8f17d25e2c9868095b75b2606b64fea9909cbb7d
Juliano Barbosa <Julianomb@gmail.com>
2016-05-28 12:01:19 -0300

    Persiste a inscricao

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionMOdelTest(TestCase):
+class SubscriptionModelTest(TestCase):
     def setUp(self):
         self.obj = Subscription(
             name='Henrique Bastos',
